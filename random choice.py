e00531fc68ed71170d3fcd36020bf847aca8d797
yan-hrushchynskyy <46781883+yan-hrushchynskyy@users.noreply.github.com>
2019-09-27 02:00:43 +0300

    add work hard

--- random choice.py
+++ random choice.py
@@ -1,3 +1,3 @@
 import random
-my_stuff = ['play with Eva', 'learn Python', 'sleep', 'read some awsome book', 'drink beer with friends', 'drink wine with Masha']
+my_stuff = ['play with Eva', 'learn Python', 'sleep', 'read some awsome book', 'drink beer with friends', 'drink wine with Masha', 'work hard']
 print(random.choice(my_stuff))
